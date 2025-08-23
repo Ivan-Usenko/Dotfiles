c2084a0faa5a4726ccfa4592772cbe17bcb2e7c3
Ivan Usenko <iusenko@gmail.com>
2025-08-23 15:23:29 +0300

    Base

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set({"v", "n", "x"}, "<leader>y", '"+y')
 vim.keymap.set({"v", "n", "x"}, "<leader>d", '"+d')
+vim.keymap.set("n", "K", "i<Cr><Esc>")
