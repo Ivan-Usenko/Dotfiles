302e914fb796a4135cc974c84d086f3a35c0734a
Ivan Usenko <iusenko@gmail.com>
2025-08-24 18:34:25 +0300

    Neovim completions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -4,7 +4,7 @@ return {
         opts = {},
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "mason-org/mason-lspconfig.nvim",
@@ -15,20 +15,26 @@ return {
         },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls", "pyright" },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
 
             vim.keymap.set("n", "I", vim.lsp.buf.hover)
             vim.keymap.set("n", "gd", vim.lsp.buf.definition)
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-        end
-    }
+        end,
+    },
 }
